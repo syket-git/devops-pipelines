a34cdfd6f0de73fac566fc19efa0b8040626f17e
Syket Bhattachergee <syketweb@gmail.com>
2025-10-19 10:54:05 +0600

    feat: add ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello DevOps");
+console.log("I'm Learning CI/CD using github actions");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting test....");
+setTimeout(() => console.log("WAITING 3 seconds...."), 3000);
+console.log("Test completed....");
